31a1f690e7ed67dfc69498ad3bc8c4baa36a1ab4
jaylineweb <darkjay118@gmail.com>
2024-07-02 14:08:34 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-// 랜덤번호 지정
-// 유저가 번호를 입력한다. 그리고 go라는 버튼을 누름
-// 만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
-// 랜덤번호 < 유저번호 Down!!
-// 랜덤번호 > 유저번호 Up!!
-// Reset 버튼을 누르면 게임이 리셋된다.
-// 5번의 기회를 다 쓰면 게임이 끝난다(더이상 추측 불가, 버튼이 disable)
-// 유저가 1~100 범위 밖의 숫자를 입력하면 알려준다. 기회를 깎지 않는다.
-// 유저가 이미 입력한 숫자를 또 입력하면, 알려준다. 기회를 깎지 않는다.
-
 let computerNum = 0; //랜덤번호 지정
 let playButton = document.getElementById('play-button');
 let userInput = document.getElementById('user-input');
@@ -18,45 +8,39 @@ let gameOver = false;
 let chanceArea = document.getElementById('chance-area');
 let history = [];
 
-playButton.addEventListener('click',play);
-resetButton.addEventListener('click',reset);
-userInput.addEventListener('focus',function(){
-    userInput.value='';
+playButton.addEventListener('click', play);
+resetButton.addEventListener('click', reset);
+userInput.addEventListener('focus', function(){
+    userInput.value = '';
 });
 
-function pickRandomNum(){
-    computerNum = Math.floor(Math.random()*100) + 1;
-    console.log("정답",computerNum)
+function pickRandomNum() {
+    computerNum = Math.floor(Math.random() * 100) + 1;
+    console.log("정답", computerNum);
 }
 
-function play(){
-    let userValue = userInput.value;
+function play() {
+    let userValue = parseInt(userInput.value);
 
-    if(userValue < 1 || userValue > 100){
+    if (userValue < 1 || userValue > 100) {
         resultArea.textContent = '1과 100 사이 숫자를 입력해 주세요.';
         return;
     }
 
-    if(history.includes(userValue)){
-        resultArea.textContent='이미 입력한 숫자입니다. 다른 숫자를 입력해주세요.'
+    if (history.includes(userValue)) {
+        resultArea.textContent = '이미 입력한 숫자입니다. 다른 숫자를 입력해주세요.';
         return;
     }
 
-    chances -- ;
-    chanceArea.textContent = `남은 기회:${chances}번`;
-    console.log('chance',chances);
-    // 랜덤번호 < 유저번호 Down!!
-    if(computerNum < userValue){
-        // console.log("Down!!");
+    chances--;
+    chanceArea.textContent = `남은 기회: ${chances}번`;
+    console.log('chance', chances);
+
+    if (computerNum < userValue) {
         resultArea.textContent = "내려!!";
-    }
-    // 랜덤번호 > 유저번호 Up!!
-    else if(computerNum > userValue){
-        // console.log("Up!!");
+    } else if (computerNum > userValue) {
         resultArea.textContent = "올려!!";
-    }
-    else{
-        // console.log('맞췄습니다!');
+    } else {
         resultArea.textContent = "맞췄..네..?";
         gameOver = true;
     }
@@ -64,18 +48,24 @@ function play(){
     history.push(userValue);
     console.log(history);
 
-    if(chances < 1){
+    if (gameOver) {
+        playButton.disabled = true;
+    } else if (chances < 1) {
         gameOver = true;
-    }
-    if(gameOver == true){
+        resultArea.textContent = `게임 오버! 정답은 ${computerNum}였습니다.`;
         playButton.disabled = true;
     }
 }
-function reset(){
+
+function reset() {
     userInput.value = '';
-
+    chances = 5;
+    gameOver = false;
+    history = [];
     pickRandomNum();
-
     resultArea.textContent = '결과값이 여기 나옵니다!';
+    chanceArea.textContent = `남은 기회: ${chances}번`;
+    playButton.disabled = false;
 }
+
 pickRandomNum();
